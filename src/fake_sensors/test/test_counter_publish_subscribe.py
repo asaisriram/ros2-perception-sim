ed6d4e0dabc4f5489bdc216edbab25add3fc6d88
asaisriram <70279710+asaisriram@users.noreply.github.com>
2026-08-27 17:26:29 +0200

    feat: add warning publish to subscriber node and update test

--- src/fake_sensors/test/test_counter_publish_subscribe.py
+++ src/fake_sensors/test/test_counter_publish_subscribe.py
@@ -7,6 +7,8 @@ from fake_sensors.counter_subscriber import CounterSubscriber
 
 import pytest
 import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Bool, Int32
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -24,11 +26,28 @@ def ros_context():
     rclpy.shutdown()
 
 
+class TestnodeSubscriber(Node):
+    """Test node created for subscription."""
+
+    def __init__(self, topic_name, msg_type):
+        """Initialize test node."""
+        super().__init__('testnode_subscriber')
+        self.topic_name = topic_name
+        self.msg_type = msg_type
+        self.msg_data = None
+        self.test_subscriber_ = self.create_subscription(
+            self.msg_type, self.topic_name, self.testnode_subscriber_callback, 10)
+
+    def testnode_subscriber_callback(self, msg):
+        """Return the msg data."""
+        self.msg_data = msg.data
+
+
 def test_counter_publisher(ros_context):
     """Verify that the publisher increments its counter."""
     count = 5
     node_publisher = CounterPublisher()
-    for _ in range(count):
+    for each_spin in range(count):
         rclpy.spin_once(node_publisher, timeout_sec=0.2)
     assert node_publisher.counter == count
     node_publisher.destroy_node()
@@ -46,3 +65,21 @@ def test_counter_subscriber(ros_context):
     assert node_subscriber.msg_count == count
     node_publisher.destroy_node()
     node_subscriber.destroy_node()
+
+
+@pytest.mark.parametrize('value, threshold, expected', [
+    (499, 500, False),
+    (501, 500, True),
+])
+def test_publish_warning(ros_context, value, threshold, expected):
+    """Verify that the subscriber publishes warning."""
+    msg = Int32()
+    node_subscriber_ = CounterSubscriber()
+    testnode_subscriber_ = TestnodeSubscriber('/status/warning', Bool)
+    node_subscriber_.threshold = threshold
+    msg.data = value
+    node_subscriber_.listener_callback(msg)
+    rclpy.spin_once(testnode_subscriber_, timeout_sec=0.2)
+    assert testnode_subscriber_.msg_data == expected
+    node_subscriber_.destroy_node()
+    testnode_subscriber_.destroy_node()
